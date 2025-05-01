e2a0f67c0e161605b1c934b40ba02ffbb3e67c9a
Gizem üçüncü <gizemucuncu34@gmail.com>
2025-04-30 22:07:35 -0700

    ödev_9 full commit

---
+++ ödev_9.sql
@@ -0,0 +1,8 @@
+select city.city, country.country from city 
+inner join country on city.country_id = country.country_id;
+
+select customer.first_name, customer.last_name, payment.payment_id from customer 
+inner join payment on customer.customer_id = payment.customer_id;
+
+select customer.first_name, customer.last_name, rental.rental_id from customer 
+inner join rental on customer.customer_id = rental.customer_id;
